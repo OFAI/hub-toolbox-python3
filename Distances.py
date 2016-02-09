79ac63be11aa7f7bac2d17e08808294bece434e2
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-02-09 17:18:11 +0100

    Dead code removal

--- Distances.py
+++ Distances.py
@@ -21,16 +21,7 @@ def cosine_distance(X):
 
 def euclidean_distance(X):
     """Calculate the euclidean distances between all pairs of vectors in X."""
-    #===========================================================================
-    # # TODO vectorize to gain speed
-    # D = np.zeros((X.shape[0], X.shape[0]))
-    # for i in range(len(X)):
-    #     for j in range(i+1, len(X)):
-    #         D[i, j] = np.linalg.norm(X[i]-X[j])
-    # D[D<0] = 0  # distances must not be negative
-    # D += D.T    # set symmetric distances (dist(x_j, x_i) = dist(x_i, x_j))
-    #===========================================================================
-    # using scipy
+
     D = cdist(X, X, 'euclidean')
     return D
 

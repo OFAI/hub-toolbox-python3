14770e629e83b5a9aaf9f16240b008bf0d7620ba
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-08-04 14:53:45 +0200

    Added doc strings, deprecated class structure

--- hub_toolbox/SharedNN.py
+++ hub_toolbox/SharedNN.py
@@ -14,18 +14,77 @@ Contact: <roman.feldbauer@ofai.at>
 """
 
 import numpy as np
+import sys
+
+def shared_nearest_neighbors(D:np.array, k:int=10, metric='distance'):
+    """Transform distance matrix using shared nearest neighbors [1].
+    
+    SNN similarity is based on computing the overlap between the k nearest 
+    neighbors of two objects. SNN approaches try to symmetrize nearest neighbor 
+    relations using only rank and not distance information [2].
+    
+    Parameters:
+    -----------
+    D : np.array
+        The n x n symmetric distance (similarity) matrix.
+        
+    k : int, optional (default: 10)
+        Neighborhood radius: The k nearest neighbors are used to calculate SNN.
+        
+    metric : {'distance', 'similarity'}, optional (default: 'distance')
+        Define, whether the matrix 'D' is a distance or similarity matrix
+
+    Returns:
+    --------
+    D_snn : ndarray
+        Secondary distance SNN matrix
+        
+    See also:
+    ---------    
+    [1] R. Jarvis and E. A. Patrick, “Clustering using a similarity measure 
+    based on shared near neighbors,” IEEE Transactions on Computers, 
+    vol. 22, pp. 1025–1034, 1973.
+    
+    [2] Flexer, A., & Schnitzer, D. (2013). Can Shared Nearest Neighbors 
+    Reduce Hubness in High-Dimensional Spaces? 2013 IEEE 13th International 
+    Conference on Data Mining Workshops, 460–467. 
+    http://doi.org/10.1109/ICDMW.2013.101
+    """
+    if D.shape[0] != D.shape[1]:
+        raise TypeError("Distance/similarity matrix is not quadratic.")
+    if metric == 'distance':
+        sort_order = 1
+    elif metric == 'similarity':
+        sort_order = -1
+    else:
+        raise ValueError("Parameter 'metric' must be "
+                         "'distance' or 'similarity'.")
+    # need to copy matrix, because it is modified later
+    self_D = D.copy()
+    
+    n = np.shape(self_D)[0]
+    z = np.zeros_like(self_D, bool)
+    for i in range(n):
+        di = self_D[i, :]
+        di[i] = np.inf
+        nn = np.argsort(di)[::sort_order]
+        z[i, nn[0:k]] = 1
+        
+    D_snn = np.zeros_like(self_D)
+    for i in range(n):
+        zi = z[i, :]
+        j_idx = np.arange(i+1, n)
+        
+        # using broadcasting
+        Dij = np.sum(np.logical_and(zi, z[j_idx, :]), 1)
+        
+        D_snn[i, j_idx] = 1 - Dij / k
+        D_snn[j_idx, i] = D_snn[i, j_idx]
+
+    return D_snn
 
 class SharedNN():
-    """
-    Transforms the given distance matrix into new one using a shared nearest
-    neighbor transform with the given neighborhood radius k.
-    
-    Usage:
-    snn = SharedNN(D, k)
-    D_snn = snn.perform_snn()
-        - Use SNN with a neighborhood radius of k on the
-        given distance matrix. The new distances are returned in D_snn.
-    """
+    """ DEPRECATED class."""
     
     def __init__(self, D, k=10, isSimilarityMatrix=False):
         self.D = np.copy(D)
@@ -38,23 +97,10 @@ class SharedNN():
     def perform_snn(self):
         """Transform distance matrix using shared nearest neighbor."""
         
-        n = np.shape(self.D)[0]
-        z = np.zeros_like(self.D, bool)
-        for i in range(n):
-            di = self.D[i, :]
-            di[i] = np.inf
-            nn = np.argsort(di)[::self.sort_order]
-            z[i, nn[0:self.k]] = 1
-            
-        Dsnn = np.zeros_like(self.D)
-        for i in range(n):
-            zi = z[i, :]
-            j_idx = np.arange(i+1, n)
-            
-            # using broadcasting
-            Dij = np.sum(np.logical_and(zi, z[j_idx, :]), 1)
-            
-            Dsnn[i, j_idx] = 1 - Dij / self.k
-            Dsnn[j_idx, i] = Dsnn[i, j_idx]
-    
-        return Dsnn
+        print("DEPRECATED: Please use SharedNN.shared_nearest_neighbors() "
+              "instead.", file=sys.stderr)
+        if self.sort_order == -1:
+            metric = 'similarity'
+        else:
+            metric = 'distance'
+        return shared_nearest_neighbors(self.D, self.k, metric)

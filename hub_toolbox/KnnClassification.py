3c8fdf2533ce7c622b3650900fc85e2a33e623a9
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-08-08 14:13:46 +0200

    Refactoring.
    Function renamed to score(), added docstring, deprecated class structure

--- hub_toolbox/KnnClassification.py
+++ hub_toolbox/KnnClassification.py
@@ -15,11 +15,151 @@ Contact: <roman.feldbauer@ofai.at>
 
 import numpy as np
 from scipy.sparse.base import issparse
+from hub_toolbox import Logging
+import sys
 
+def score(D:np.ndarray, target:np.ndarray, k:int=5, metric:str='distance',
+          test_set_ind:np.ndarray=None, verbose:int=0):
+    """Perform k-nearest neighbor classification.
+    
+    Use the (n x n) symmetric distance matrix (D) and target class 
+    labels (target) to perform a k-NN experiment (leave-one-out 
+    cross-validation or evaluation of test set; see parameter 'test_set_ind').
+    Ties are broken by the nearest neighbor.
+    
+    Parameters:
+    -----------
+    D : ndarray
+        The n x n symmetric distance (similarity) matrix.
+    
+    target : ndarray
+        The n x 1 target class labels (ground truth).
+    
+    k : int, optional (default: 5)
+        Neighborhood size for k-NN classification.
+    
+    metric : {'distance', 'similarity'}, optional (default: 'distance')
+        Define, whether matrix 'D' is a distance or similarity matrix
+    
+    test_sed_ind : ndarray, optional (default: None)
+        Define data points to be hold out as part of a test set. Can be:
+        - None : Perform a LOO-CV experiment
+        - ndarray : Hold out points indexed in this array as test set. Fit 
+                    model to remaining data. Evaluate model on test set.
+    
+    verbose : int, optional (default: 0)
+        Increasing level of output (progress report).
+    
+    Returns:
+    --------
+    acc : float
+        Classification accuracy
+    corr : ndarray
+        Raw vector of correctly classified items
+    cmat : ndarray
+        Confusion matrix
+    """
+    
+    # Check input sanity
+    log = Logging.ConsoleLogging()
+    if D.shape[0] != D.shape[1]:
+        raise TypeError("Distance/similarity matrix is not quadratic.")
+    if target.size != D.shape[0]:
+        raise TypeError("Target vector length does not match number of points.")
+    if metric == 'distance':
+        d_self = np.inf
+        sort_order = 1
+    elif metric == 'similarity':
+        d_self = -np.inf
+        sort_order = -1
+    else:
+        raise ValueError("Parameter 'metric' must be 'distance' or "
+                         "'similarity'.")
+    # Copy, because data is changed
+    D = D.copy()
+    
+    if verbose:
+        log.message("Start k-NN experiment.")
+    # Handle LOO-CV vs. test set mode
+    if test_set_ind is None:
+        n = D.shape[0]
+        test_set_ind = range(n)    # dummy 
+        train_set_ind = n   # dummy
+    else:  
+        # number of points to be classified
+        n = test_set_ind.size
+        # Indices of training examples
+        train_set_ind = np.setdiff1d(np.arange(n), test_set_ind)
+    # Why would there be a need for more than one k?
+    k_length = np.size(k)
+        
+    acc = np.zeros((k_length, 1))
+    corr = np.zeros((D.shape[0], k_length))
+        
+    cl = np.sort(np.unique(target))
+    cmat = np.zeros((len(cl), len(cl)))
+    
+    classes = target.copy()
+    for idx in range(len(cl)):
+        # change labels to 0, 1, ..., len(cl)-1
+        classes[target == cl[idx]] = idx
+        
+    cl = range(len(cl))
+    
+    # Classify each point in test set
+    for i in test_set_ind:
+        seed_class = classes[i]
+        
+        if issparse(D):
+            row = D.getrow(i).toarray().ravel()
+        else:
+            row = D[i, :]
+        row[i] = d_self
+        
+        # Sort points in training set according to distance
+        # Randomize, in case there are several points of same distance
+        # (this is especially relevant for SNN rescaling)
+        rp = train_set_ind
+        rp = np.random.permutation(rp)
+        d2 = row[rp]
+        d2idx = np.argsort(d2, axis=0)[::sort_order]
+        idx = rp[d2idx]      
+        
+        # More than one k?
+        for j in range(k_length):
+            nn_class = classes[idx[0:k[j]]]
+            cs = np.bincount(nn_class.astype(int))
+            max_cs = np.where(cs == np.max(cs))[0]
+            
+            # "tie": use nearest neighbor
+            if len(max_cs) > 1:
+                if seed_class == nn_class[0]:
+                    acc[j] += 1/n 
+                    corr[i, j] = 1
+                cmat[seed_class, nn_class[0]] += 1       
+            # majority vote
+            else:
+                if cl[max_cs] == seed_class:
+                    acc[j] += 1/n
+                    corr[i, j] = 1
+                cmat[seed_class, cl[max_cs]] += 1
+                       
+    if verbose:
+        log.message("Finished k-NN experiment.")
+        
+    return acc, corr, cmat
+
+###############################################################################
+#
+# DEPRECATED class
+#
 class KnnClassification():
-    """Perform k-nearest neighbor classification."""
+    """DEPRECATED"""
     
     def __init__(self, D, classes, k, isSimilarityMatrix=False):
+        """DEPRECATED"""
+        print("DEPRECATED: Please use KnnClassification.score() instead.", 
+              file=sys.stderr)
         if issparse(D):
             self.D = D
         else:
@@ -39,144 +179,18 @@ class KnnClassification():
         assert D.shape[0] == len(classes)
         
     def perform_knn_classification(self):
-        """Performs k-nearest neighbor classification.
-        
-        Use the distance matrix D (NxN) and the classes and perform a 
-        k-NN experiment. Ties are broken by the nearest neighbor.
-        The classification accuracy is returned in acc. 
-        corr is a raw vector of the correctly classified items. 
-        cmat is the confusion matrix.
-        """
-        
-        # Why would there be a need for more than one k?
-        k_length = np.size(self.k)
+        """DEPRECATED"""
+        if self.isSimilarityMatrix:
+            metric = 'similarity'
+        else:
+            metric = 'distance'
+        return score(self.D, self.classes, self.k, metric, None, 0)
             
-        acc = np.zeros( (k_length, 1) )
-        corr = np.zeros( (self.D.shape[0], k_length) )
-        
-        n = np.size(self.D, 1)
-        
-        cl = np.sort(np.unique(self.classes))
-        cmat = np.zeros( (len(cl), len(cl)) )
-        
-        classes = self.classes
-        for idx in range(len(cl)):
-            classes[self.classes == cl[idx]] = idx
-            
-        cl = range(len(cl))
-        
-        for i in range(n):
-            seed_class = classes[i]
-            
-            if issparse(self.D):
-                row = self.D[i, :].toarray().ravel()
-            else:
-                row = self.D[i, :]
-            
-            row[i] = self.self_value
-            
-            # Randomize, in case there are several points of same distance
-            # (this is especially relevant for SNN rescaling)
-            rp = np.indices( (np.size(self.D, 1), ) )[0]
-            rp = np.random.permutation(rp)
-            d2 = row[rp]
-            d2idx = np.argsort(d2, axis=0)[::self.sort_order]
-            idx = rp[d2idx]      
-            
-            # OLD code, non-randomized
-            #idx = np.argsort(row)
-            
-            # More than one k?
-            for j in range(k_length):
-                nn_class = classes[idx[0:self.k[j]]] #smallest dist/highest sim
-                cs = np.bincount(nn_class.astype(int))
-                max_cs = np.where(cs == np.max(cs))[0]
-                
-                # "tie": use nearest neighbor
-                if len(max_cs) > 1:
-                    if seed_class == nn_class[0]:
-                        acc[j] += 1/n 
-                        corr[i, j] = 1
-                    cmat[seed_class, nn_class[0]] += 1       
-                # majority vote
-                else:
-                    if cl[max_cs] == seed_class:
-                        acc[j] += 1/n
-                        corr[i, j] = 1
-                    cmat[seed_class, cl[max_cs]] += 1
-                           
-        return acc, corr, cmat
-    
     def perform_knn_classification_with_test_set(self, test_set_mask=None):
-        """Performs k-nearest neighbor classification.
-        
-        Use the distance matrix D (NxN) and the classes and perform a 
-        k-NN experiment. Ties are broken by the nearest neighbor.
-        The classification accuracy is returned in acc. 
-        corr is a raw vector of the correctly classified items. 
-        cmat is the confusion matrix.
-        """        
-        # Indices of training examples
-        train_set_mask = np.setdiff1d(np.arange(self.D.shape[0]), test_set_mask)
-        # Why would there be a need for more than one k?
-        k_length = np.size(self.k)
-            
-        acc = np.zeros( (k_length, 1) )
-        corr = np.zeros( (self.D.shape[0], k_length) )
-        
-        # number of points to be classified
-        #n = np.size(self.D, 1)
-        n = np.size(test_set_mask)
-        
-        cl = np.sort(np.unique(self.classes))
-        cmat = np.zeros( (len(cl), len(cl)) )
-        
-        classes = self.classes
-        for idx in range(len(cl)):
-            classes[self.classes == cl[idx]] = idx
-            
-        cl = range(len(cl))
-        
-        # Classify each point in test set
-        #for i in range(n):
-        for i in test_set_mask:
-            seed_class = classes[i]
-            
-            if issparse(self.D):
-                row = self.D[i, :].toarray().ravel()
-            else:
-                row = self.D[i, :]
-            row[i] = self.self_value
-            
-            # Sort points in training set according to distance
-            # Randomize, in case there are several points of same distance
-            # (this is especially relevant for SNN rescaling)
-            rp = train_set_mask
-            rp = np.random.permutation(rp)
-            d2 = row[rp]
-            d2idx = np.argsort(d2, axis=0)[::self.sort_order]
-            idx = rp[d2idx]      
-            
-            # OLD code, non-randomized
-            #idx = np.argsort(row)
-            
-            # More than one k?
-            for j in range(k_length):
-                nn_class = classes[idx[0:self.k[j]]]
-                cs = np.bincount(nn_class.astype(int))
-                max_cs = np.where(cs == np.max(cs))[0]
-                
-                # "tie": use nearest neighbor
-                if len(max_cs) > 1:
-                    if seed_class == nn_class[0]:
-                        acc[j] += 1/n 
-                        corr[i, j] = 1
-                    cmat[seed_class, nn_class[0]] += 1       
-                # majority vote
-                else:
-                    if cl[max_cs] == seed_class:
-                        acc[j] += 1/n
-                        corr[i, j] = 1
-                    cmat[seed_class, cl[max_cs]] += 1
-                           
-        return acc, corr, cmat
+        """DEPRECATED"""  
+        if self.isSimilarityMatrix:
+            metric = 'similarity'
+        else:
+            metric = 'distance'
+        return score(self.D, self.classes, self.k, metric, test_set_mask, 0)
+    

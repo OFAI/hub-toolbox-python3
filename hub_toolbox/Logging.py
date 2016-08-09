9b988226024143b14205ec1e35585881ac78aa9f
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-08-09 12:27:49 +0200

    added docstrings; added scaffold for FileLogging (still not implemented)

--- hub_toolbox/Logging.py
+++ hub_toolbox/Logging.py
@@ -14,29 +14,59 @@ Contact: <roman.feldbauer@ofai.at>
 """
 
 import sys, time
+from abc import ABCMeta, abstractmethod
 
-class ConsoleLogging():
+class Logging(metaclass=ABCMeta):
+    """Base class for time-stamped logging.
     
+    Do not instantiate this class, but ConsoleLogging or FileLogging!
+    """
     @property
     def _current_time(self):
+        """Formatted time stamp"""
         return time.strftime('%Y-%m-%d %H:%M:%S')
     
+    @abstractmethod
+    def message(self):
+        ...
+    @abstractmethod
+    def warning(self):
+        ...
+    @abstractmethod
+    def error(self):
+        ...
+    
+class ConsoleLogging(Logging):
+    """Convenience functions for time-stamped logging to the console"""
+    
     def message(self, *objs, flush=True):
+        """Log normal program function"""
         print(self._current_time, 'INFO:', *objs)
         if flush:
             sys.stdout.flush()
         
     def warning(self, *objs, flush=True):
+        """Log warning (program can still continue)"""
         print(self._current_time, 'WARNING:', *objs, file=sys.stderr)
         if flush:
             sys.stderr.flush()
         
     def error(self, *objs, flush=True):
+        """Log error (program fails)"""
         print(self._current_time, 'ERROR:', *objs, file=sys.stderr)
         if flush:
             sys.stderr.flush()
+            
+class FileLogging(ConsoleLogging):
+    """Convenience functions for time-stamped logging to a file"""
+    
+    def __init__(self):
+        """Not implemented"""
+        self.warning("FileLogging not yet implemented, will print to "
+                     "console anyway.")
         
 if __name__ == '__main__':
+    """Simple test of this module"""
     log = ConsoleLogging()
     log.message('This module supplies functions for printing and logging.')
     log.message('Examples:')
@@ -46,4 +76,12 @@ if __name__ == '__main__':
     log.error('This is an error!')
     sys.stderr.flush()
     time.sleep(0.01)
-    log.message('You should have got three messages on stdout and two on stderr.')
+    log.message('You should have got three messages on stdout and '
+                'two on stderr.')
+    log = FileLogging()
+    log.message('Still written to console, until implemented.')
+    try:
+        log = Logging()
+    except TypeError as e:
+        log.warning('Must not instantiate Logging(), got exception:\n', e)
+        

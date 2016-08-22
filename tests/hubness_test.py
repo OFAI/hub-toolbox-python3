eda086fccae0e9591909a3ace9ed92f0cfc4bbac
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-08-22 15:34:18 +0200

    additional unit test for hubness

--- tests/hubness_test.py
+++ tests/hubness_test.py
@@ -12,8 +12,10 @@ Austrian Research Institute for Artificial Intelligence (OFAI)
 Contact: <roman.feldbauer@ofai.at>
 """
 import unittest
+import numpy as np
 from scipy.spatial.distance import squareform
 from hub_toolbox.Hubness import hubness
+from hub_toolbox.Distances import euclidean_distance
 
 class TestHubness(unittest.TestCase):
     """Test hubness calculations"""
@@ -30,6 +32,34 @@ class TestHubness(unittest.TestCase):
         """Test hubness against ground truth calc on spreadsheet"""
         Sk5, _, _ = hubness(self.dist, k=2)
         return self.assertAlmostEqual(Sk5, self.hubness_truth, places=10)
+
+    def test_hubness_return_values_are_self_consistent(self):
+        """Test that the three returned values fit together"""
+        np.random.seed(626)
+        points = 200
+        dim = 500
+        vector = 99. * (np.random.rand(points, dim) - 0.5)
+        dist = euclidean_distance(vector)
+        k = 10
+        Sk10, Dk10, Nk10 = hubness(dist, k=k)
+        # Dk is just checked for correct shape
+        correct_dim_Dk10 = Dk10.shape == (k, points)
+        # Count k-occurence (different method than in module)
+        Dk10 = Dk10.ravel()
+        Nk10_true = np.zeros(points, dtype=int)
+        for i in range(points):
+            Nk10_true[i] = (Dk10 == i).sum()
+        correct_Nk10 = np.all(Nk10 == Nk10_true)
+        # Calculate skewness (different method than in module)
+        x0 = Nk10 - Nk10.mean()
+        s2 = (x0**2).mean()
+        m3 = (x0**3).mean()
+        s = m3 / (s2**1.5)
+        Sk10_true = s
+        correct_Sk10 = Sk10 == Sk10_true
+        return self.assertTrue(correct_dim_Dk10 
+                               and correct_Nk10 
+                               and correct_Sk10)
 
 if __name__ == "__main__":
     unittest.main()

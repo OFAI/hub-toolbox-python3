953f47312a37c936035cc004ddf8ec8116ce3d10
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-08-31 10:27:14 +0200

    fix test

--- tests/hubnessanalysis_test.py
+++ tests/hubnessanalysis_test.py
@@ -42,7 +42,8 @@ class TestHubnessAnalysis(unittest.TestCase):
 
     def test_all_sec_dist_have_header(self):
         ha_sec_dist = set(HubnessAnalysis.SEC_DIST.keys())
-        header_sec_dist = set(HubnessAnalysis()._header.keys())
+        p = (np.ones(1), np.ones(1), np.zeros(1)) # dummy params
+        header_sec_dist = set(HubnessAnalysis.HubnessAnalysis(p)._header.keys())
         n_sec_dist = len(ha_sec_dist)
         n_intersection = len(ha_sec_dist & header_sec_dist)
         return self.assertEqual(n_sec_dist, n_intersection)

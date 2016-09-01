c0249f3129286ac2650aca83821ccfeefc63d442
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-09-01 11:46:37 +0200

    update init
    make sure that numpy and scipy can be imported (error otherwise)
    
    make sure scikit-learn can be imported (warning otherwise)
    
    add relative imports (to enable the examples in the readme)

--- hub_toolbox/__init__.py
+++ hub_toolbox/__init__.py
@@ -12,3 +12,39 @@ The HUB TOOLBOX is licensed under the terms of the GNU GPLv3.
 Austrian Research Institute for Artificial Intelligence (OFAI)
 Contact: <roman.feldbauer@ofai.at>
 """
+
+try:
+    import numpy
+    import scipy
+    del numpy
+    del scipy
+except ImportError:  # pragma: no cover
+    raise ImportError("Could not import numpy and/or scipy.\n"
+                      "Please make sure you install the following Python3 "
+                      "packages: numpy, scipy and scikit-learn.\n"
+                      "See the installation docs for more details:"
+                      "http://hub-toolbox-python3.readthedocs.io/en/latest/"
+                      "user/installation.html#numpy-scipy-scikit-learn")
+
+try:
+    import sklearn
+    del sklearn
+except ImportError:  # pragma: no cover
+    print("Could not import scikit-learn. While most modules of the Hub "
+          "Toolbox do not require it, it is still advised to install scikit-"
+          "learn for Python3 to enable all functionality and unit tests.")
+
+from . import Centering
+from . import Distances
+from . import GoodmanKruskal
+from . import Hubness_parallel
+from . import Hubness
+from . import HubnessAnalysis
+from . import IntrinsicDim
+from . import IO
+from . import KnnClassification
+from . import LocalScaling
+from . import Logging
+from . import MutualProximity
+from . import MutualProximity_parallel
+from . import SharedNN

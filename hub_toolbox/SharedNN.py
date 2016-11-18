b1bdab7a6fcc8790dcc65c341e4c301a74fd7898
Roman Feldbauer <roman.feldbauer@ofai.at>
2016-11-18 16:53:15 +0100

    added simhubIN draft version

--- hub_toolbox/SharedNN.py
+++ hub_toolbox/SharedNN.py
@@ -184,6 +184,120 @@ def shared_nearest_neighbors(D:np.ndarray, k:int=10, metric='distance'):
     np.fill_diagonal(D_snn, self_value)
     return D_snn
 
+def simhubIN(D:np.ndarray, train_ind:np.ndarray=None, 
+             test_ind:np.ndarray=None, s:int=50, return_distances:bool=True):
+    """Calculate dissimilarity based on hubness-aware SNN distances [1]_.
+    
+    Parameters
+    ----------
+    D : ndarray
+        The ``n x s`` distance or similarity matrix, where ``n`` and ``s``
+        are the dataset and sample size, respectively.
+
+    train_ind : ndarray, optional, default: None
+        The index array that determines, to which data points the columns in
+        `D` correspond. Not required, if `D` is a quadratic all-against-all
+        distance matrix.
+
+    test_ind : ndarray, optional, default: None
+        Define data points to be hold out as part of a test set. Can be:
+
+        - None : Rescale all distances
+        - ndarray : Hold out points indexed in this array as test set.
+
+    s : int, optional, default: 50
+        Neighborhood size. Can be optimized as to minimize hubness.
+
+    return_distances : bool, optional, default: True
+        If True, return distances (1 - similarities).
+        Otherwise return similarities.
+
+    Returns
+    -------
+    D_shi : ndarray
+        Secondary distance (simhubIN) matrix.
+        
+    References
+    ----------
+    .. [1] Tomašev, N., Mladenić, D., Tomasev, N., & Mladenić, D. (2012).
+           Hubness-aware shared neighbor distances for high-dimensional
+           $$k$$ -nearest neighbor classification.
+           Knowledge and Information Systems, 39(1), 89–122.
+           http://doi.org/10.1007/s10115-012-0607-5
+    """
+    if train_ind is None:
+        IO._check_distance_matrix_shape(D)
+    else:
+        IO._check_sample_shape_fits(D, train_ind)
+    # Assuming distances in D
+    self_value = 0.
+    sort_order = 1
+    exclude = np.inf
+    distance = D.copy()
+    n, s = distance.shape
+    if test_ind is None:
+        n_ind = range(n)    
+    else:
+        n_ind = test_ind
+    # Exclude self distances
+    if train_ind is None:
+        np.fill_diagonal(distance, exclude)
+    else:
+        for j, sample in enumerate(train_ind):
+            distance[sample, j] = exclude
+
+    knn = np.zeros_like(distance, bool)
+    
+    # find nearest neighbors for each point
+    for i in range(n):
+        di = distance[i, :]
+        nn = np.argsort(di)[::sort_order]
+        knn[i, nn[:s]] = True
+    del distance
+    
+    # "Occurence informativeness"
+    occ_inf_knn = knn.copy()
+    for j, sample in enumerate(train_ind):
+        # treat x as its own 0th neighbor to avoid div/0
+        occ_inf_knn[sample, j] = True
+    N_s = occ_inf_knn.sum(axis=0)
+    I_n = np.log(n / N_s)
+    del occ_inf_knn
+
+    # simhub calculation
+    D_shi = np.zeros_like(D)
+    if s < 2000: # using vectorization and broadcasting
+        for i in n_ind:
+            x = np.logical_and(knn[i, :], knn[train_ind, :])
+            D_shi[i, :] = \
+                np.sum(x * I_n, axis=1)
+    else: # use non-vectorized loops
+        for i in n_ind:
+            for j in range(s):
+                x = np.logical_and(knn[i, :], knn[j, :])
+                D_shi[i, j] = \
+                    np.sum(x * I_n)
+    del knn
+    # Normalization to [0, 1] range
+    D_shi /= (s * np.log(n))
+
+    # Convert to distances
+    if return_distances:
+        D_shi *= -1
+        D_shi += 1
+    else:
+        self_value = 1
+
+    # Ensure correct self distances and return sec. dist. matrix
+    if test_ind is None:
+        np.fill_diagonal(D_shi, self_value)
+        return D_shi
+    else:
+        for j, sample in enumerate(train_ind):
+            D_shi[sample, j] = self_value
+        return D_shi[test_ind]
+
+
 class SharedNN(): # pragma: no cover
     """
     .. note:: Deprecated in hub-toolbox 2.3

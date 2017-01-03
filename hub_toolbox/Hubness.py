8cb6f5d61a1cd2f88c6c9a81e7ace0515b372bb9
feldbauer <roman.feldbauer@ofai.at>
2017-01-03 11:19:57 +0100

    Merged parallel hubness code into Hubness module.
    Deprecated Hubness_parallel module (to be removed in 3.0)
    
    Removed EOL whitespaces

--- hub_toolbox/Hubness.py
+++ hub_toolbox/Hubness.py
@@ -13,13 +13,171 @@ Austrian Research Institute for Artificial Intelligence (OFAI)
 Contact: <roman.feldbauer@ofai.at>
 """
 
+import sys
 import numpy as np
 from scipy import stats
 from scipy.sparse.base import issparse
+import multiprocessing as mp
 from hub_toolbox import IO, Logging
-import sys
 
-def hubness(D:np.ndarray, k:int=5, metric='distance', verbose:int=0):
+def hubness(D:np.ndarray, k:int=5, metric='distance',
+            verbose:int=0, n_jobs:int=1):
+    """Compute hubness of a distance matrix.
+
+    Hubness [1]_ is the skewness of the `k`-occurrence histogram (reverse
+    nearest neighbor count, i.e. how often does a point occur in the
+    `k`-nearest neighbor lists of other points).
+
+    Parameters
+    ----------
+    D : ndarray
+        The ``n x n`` symmetric distance (similarity) matrix.
+
+    k : int, optional (default: 5)
+        Neighborhood size for `k`-occurence.
+
+    metric : {'distance', 'similarity'}, optional (default: 'distance')
+        Define, whether matrix `D` is a distance or similarity matrix
+
+    verbose : int, optional (default: 0)
+        Increasing level of output (progress report).
+
+    n_jobs : int, optional (default: 1)
+        Number of parallel processes spawned for hubness calculation.
+        Value 1 (default): One process (not using multiprocessing)
+        Value (-1): As many processes as number of available CPUs.
+
+    Returns
+    -------
+    S_k : float
+        Hubness (skewness of `k`-occurence distribution)
+    D_k : ndarray
+        `k`-nearest neighbor lists
+    N_k : ndarray
+        `k`-occurence list
+
+    References
+    ----------
+    .. [1] Radovanović, M., Nanopoulos, A., & Ivanović, M. (2010).
+           Hubs in Space : Popular Nearest Neighbors in High-Dimensional Data.
+           Journal of Machine Learning Research, 11, 2487–2531. Retrieved from
+           http://jmlr.csail.mit.edu/papers/volume11/radovanovic10a/
+           radovanovic10a.pdf
+    """
+    # Don't use multiprocessing environment when using only one job
+    if n_jobs == 1:
+        return _hubness_no_multiprocessing(D=D, k=k, metric=metric,
+                                           verbose=verbose)
+    log = Logging.ConsoleLogging()
+    IO._check_distance_matrix_shape(D)
+    IO._check_valid_metric_parameter(metric)
+    if metric == 'distance':
+        d_self = np.inf
+        sort_order = 1
+    if metric == 'similarity':
+        d_self = -np.inf
+        sort_order = -1
+
+    if verbose:
+        log.message("Hubness calculation (skewness of {}-occurence)".format(k))
+
+    # Initialization
+    n = D.shape[0]
+    D = D.copy()
+    D_k = np.zeros((k, D.shape[1]), dtype=np.float32 )
+
+    if issparse(D): 
+        pass # correct self-distance must be ensured upstream for sparse
+    else:
+        # Set self dist to inf
+        np.fill_diagonal(D, d_self)
+        # make non-finite (NaN, Inf) appear at the end of the sorted list
+        D[~np.isfinite(D)] = d_self
+
+    # Parallelization
+    if n_jobs == -1: # take all cpus
+        NUMBER_OF_PROCESSES = mp.cpu_count() # @UndefinedVariable
+    else:
+        NUMBER_OF_PROCESSES = n_jobs
+    tasks = []
+
+    batches = []
+    batch_size = n // NUMBER_OF_PROCESSES
+    for i in range(NUMBER_OF_PROCESSES-1):
+        batches.append( np.arange(i*batch_size, (i+1)*batch_size) )
+    batches.append( np.arange((NUMBER_OF_PROCESSES-1)*batch_size, n) )
+
+    for idx, batch in enumerate(batches):
+        submatrix = D[batch[0]:batch[-1]+1]
+        tasks.append((_partial_hubness,
+                     (k, d_self, log, sort_order,
+                      batch, submatrix, idx, n, verbose)))
+
+    task_queue = mp.Queue()
+    done_queue = mp.Queue()
+
+    for task in tasks:
+        task_queue.put(task)
+
+    for i in range(NUMBER_OF_PROCESSES):
+        mp.Process(target=_worker, args=(task_queue, done_queue)).start()
+
+    for i in range(len(tasks)):
+        rows, Dk_part = done_queue.get()
+        D_k[:, rows[0]:rows[-1]+1] = Dk_part
+        
+    for i in range(NUMBER_OF_PROCESSES):
+        task_queue.put('STOP')
+
+    # k-occurence
+    N_k = np.bincount(D_k.astype(int).ravel())
+    # Hubness
+    S_k = stats.skew(N_k)
+
+    if verbose:
+        log.message("Hubness calculation done.", flush=True)
+
+    # return hubness, k-nearest neighbors, N occurence
+    return S_k, D_k, N_k
+        
+def _worker(work_input, work_output):
+    """A helper function for cv parallelization."""
+    for func, args in iter(work_input.get, 'STOP'):
+        result = _calculate(func, args)
+        work_output.put(result)
+
+def _calculate(func, args):
+    """A helper function for cv parallelization."""
+    return func(*args)
+
+def _partial_hubness(k, d_self, log, sort_order, 
+                     rows, submatrix, idx, n, verbose):
+    """Parallel hubness calculation: Get k nearest neighbors for all points
+    in 'rows'"""
+
+    Dk = np.zeros((k, len(rows)), dtype=np.float32)
+
+    for i, row in enumerate(submatrix):
+        if verbose and ((rows[i]+1)%10000==0 or rows[i]+1==n):
+            log.message("NN: {} of {}.".format(rows[i]+1, n), flush=True)
+        if issparse(submatrix):
+            d = row.toarray().ravel() # dense copy of one row
+        else: # normal ndarray
+            d = row
+        d[rows[i]] = d_self
+        d[~np.isfinite(d)] = d_self
+        # randomize the distance matrix rows to avoid the problem case
+        # if all numbers to sort are the same, which would yield high
+        # hubness, even if there is none
+        rp = np.random.permutation(n)
+        d2 = d[rp]
+        d2idx = np.argsort(d2, axis=0)[::sort_order]
+        Dk[:, i] = rp[d2idx[0:k]]
+
+    return [rows, Dk]
+
+def _hubness_no_multiprocessing(D:np.ndarray, k:int=5, metric='distance',
+                                verbose:int=0):
     """Compute hubness of a distance matrix.
 
     Hubness [1]_ is the skewness of the `k`-occurrence histogram (reverse

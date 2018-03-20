f9e7e2b2cf18695d67741fe3f46257b9d3c192e7
feldbauer <roman.feldbauer@ofai.at>
2018-03-20 12:17:02 +0100

    TST Approximate Hubness Reduction

--- tests/approximate_test.py
+++ tests/approximate_test.py
@@ -17,40 +17,43 @@ from hub_toolbox import approximate
 from sklearn.neighbors.classification import KNeighborsClassifier
 from sklearn.metrics.classification import accuracy_score
 
-class Test(unittest.TestCase):
+class ApproximateHRTest(unittest.TestCase):
 
 
     def setUp(self):
+        n_samples = 500
+        test_size = int(n_samples * .2)
         X, y = make_classification(
-            n_samples=1_000, n_features=1_000, n_informative=500,
+            n_samples=n_samples, n_features=1_000, n_informative=500,
             n_redundant=0, n_repeated=0, n_classes=2,
             n_clusters_per_class=10, random_state=2847356)
         X_train, X_test, y_train, y_test = train_test_split(
-            X.astype(np.float32), y.astype(np.int32), test_size=20)
+            X.astype(np.float32), y.astype(np.int32), test_size=test_size)
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
 
-        self.hr_algorithm = 'LS'
+        self.hr_algorithms = ['LS', 'NICDM', 'MP', 'MPG', 'DSL', None]
         self.n_neighbors = 5
         self.n_samples = 100
-        self.sampling_algorithm = 'LSH'
-        self.metric = 'sqeuclidean'
+        self.sampling_algorithms = ['random', 'kmeans++', 'LSH', 'HNSW', None]
+        self.metrics = ['sqeuclidean', 'cosine']
         self.n_jobs = 4
         self.verbose = 1
-
 
     def tearDown(self):
         pass
 
-
-    def testApproximateHRwithLSH(self):
-        hr = approximate.SuQHR(
-            hr_algorithm=self.hr_algorithm, n_neighbors=self.n_neighbors,
-            n_samples=self.n_samples, metric=self.metric,
-            sampling_algorithm=self.sampling_algorithm,
-            random_state=123, n_jobs=self.n_jobs, verbose=self.verbose)
+    def _approximate_hr(self, hr_algorithm, sampling_algorithm, metric):
+        hr = approximate.SuQHR(hr_algorithm=hr_algorithm,
+                               n_neighbors=self.n_neighbors,
+                               n_samples=self.n_samples,
+                               metric=metric,
+                               sampling_algorithm=sampling_algorithm,
+                               random_state=123,
+                               n_jobs=self.n_jobs,
+                               verbose=self.verbose)
         hr.fit(self.X_train, self.y_train)
         D_test_hr = hr.transform(self.X_test)
         D_test_hr = approximate.enforce_finite_distances(D_test_hr)
@@ -61,7 +64,9 @@ class Test(unittest.TestCase):
                                        metric='precomputed')
             knn.fit(D_test_hr.T, hr.y_train_)
             y_pred = knn.predict(D_test_hr)
-        else: # W/o fixed vantage points, a new classifier is required for each test object
+        else:
+            # W/o fixed vantage points, a new classifier
+            # is required for each test object
             for i, d_test in enumerate(D_test_hr):
                 d_test = d_test.reshape(-1, 1)
                 knn = KNeighborsClassifier(n_neighbors=self.n_neighbors,
@@ -70,7 +75,16 @@ class Test(unittest.TestCase):
                 knn.fit(d_test, self.y_train[hr.ind_test_[i]])
                 y_pred[i] = knn.predict(d_test.T)
         acc = accuracy_score(y_pred, self.y_test)
-        print(f'SuQHR accuracy: {acc:.2f}')
+        print(f'SuQHR ({hr_algorithm}, {sampling_algorithm}, {metric}) '
+              f'{self.n_neighbors}-NN accuracy: {acc:.2f}')
+
+    def test_approximate_hubness_reduction(self):
+        for hr_algorithm in self.hr_algorithms:
+            for sampling_algorithm in self.sampling_algorithms:
+                for metric in self.metrics:
+                    self._approximate_hr(hr_algorithm,
+                                         sampling_algorithm,
+                                         metric)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

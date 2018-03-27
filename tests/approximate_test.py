0799d77adc9a1b0dfc59fe25ddfffaf7bf5d87e3
feldbauer <roman.feldbauer@ofai.at>
2018-03-27 12:52:10 +0200

    MAINT adapt to use SuQHR.predict()

--- tests/approximate_test.py
+++ tests/approximate_test.py
@@ -5,7 +5,7 @@ This file is part of the HUB TOOLBOX available at
 https://github.com/OFAI/hub-toolbox-python3/
 The HUB TOOLBOX is licensed under the terms of the GNU GPLv3.
 
-(c) 2016-2018, Roman Feldbauer
+(c) 2018, Roman Feldbauer
 Austrian Research Institute for Artificial Intelligence (OFAI)
 Contact: <roman.feldbauer@ofai.at>
 """
@@ -14,7 +14,6 @@ import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from hub_toolbox import approximate
-from sklearn.neighbors.classification import KNeighborsClassifier
 from sklearn.metrics.classification import accuracy_score
 
 class ApproximateHRTest(unittest.TestCase):
@@ -22,9 +21,13 @@ class ApproximateHRTest(unittest.TestCase):
 
     def setUp(self):
         n_samples = 500
+        n_informative = 256
+        n_features = n_informative
         test_size = int(n_samples * .2)
         X, y = make_classification(
-            n_samples=n_samples, n_features=1_000, n_informative=500,
+            n_samples=n_samples,
+            n_features=n_features,
+            n_informative=n_informative,
             n_redundant=0, n_repeated=0, n_classes=2,
             n_clusters_per_class=10, random_state=2847356)
         X_train, X_test, y_train, y_test = train_test_split(
@@ -41,9 +44,10 @@ class ApproximateHRTest(unittest.TestCase):
         self.metrics = ['sqeuclidean', 'cosine']
         self.n_jobs = [-1, 1]
         self.verbose = 3
+        self.accu_time = 0.
 
     def tearDown(self):
-        pass
+        print(f'Accumulated time: {self.accu_time} seconds.')
 
     def _approximate_hr(self, hr_algorithm, sampling_algorithm,
                         metric, n_jobs):
@@ -56,28 +60,12 @@ class ApproximateHRTest(unittest.TestCase):
                                n_jobs=n_jobs,
                                verbose=self.verbose)
         hr.fit(self.X_train, self.y_train)
-        D_test_hr = hr.transform(self.X_test)
-        D_test_hr = approximate.enforce_finite_distances(D_test_hr)
-        y_pred = np.empty(self.X_test.shape[0])
-        if hr.fixed_vantage_pts_:
-            knn = KNeighborsClassifier(n_neighbors=self.n_neighbors,
-                                       algorithm='brute',
-                                       metric='precomputed')
-            knn.fit(D_test_hr.T, hr.y_train_)
-            y_pred = knn.predict(D_test_hr)
-        else:
-            # W/o fixed vantage points, a new classifier
-            # is required for each test object
-            for i, d_test in enumerate(D_test_hr):
-                d_test = d_test.reshape(-1, 1)
-                knn = KNeighborsClassifier(n_neighbors=self.n_neighbors,
-                                           algorithm='brute',
-                                           metric='precomputed')
-                knn.fit(d_test, self.y_train[hr.ind_test_[i]])
-                y_pred[i] = knn.predict(d_test.T)
+        y_pred = hr.predict(self.X_test)
         acc = accuracy_score(y_pred, self.y_test)
         print(f'SuQHR ({hr_algorithm}, {sampling_algorithm}, {metric}) '
               f'{self.n_neighbors}-NN accuracy: {acc:.2f}')
+        total_time = hr.time_fit_ + hr.time_transform_ + hr.time_predict_
+        self.accu_time += total_time.total.values
 
     def test_approximate_hubness_reduction(self):
         for hr_algorithm in self.hr_algorithms:
